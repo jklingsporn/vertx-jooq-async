992eae28af04039e478999873e2674180a39da4f
Jens Klingsporn <klingsporn@badmonkee.de>
2017-06-22 10:53:09 +0200

    Initial commit

---
+++ vertx-jooq-async-future/src/main/java/io/github/jklingsporn/vertx/jooq/future/async/AsyncJooqSQLClient.java
@@ -0,0 +1,49 @@
+package io.github.jklingsporn.vertx.jooq.future.async;
+
+import io.github.jklingsporn.vertx.jooq.future.async.impl.AsyncJooqSQLClientImpl;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import org.jooq.Query;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
+/**
+ * Created by jensklingsporn on 13.06.17.
+ */
+public interface AsyncJooqSQLClient {
+
+    public static AsyncJooqSQLClient create(Vertx vertx,AsyncSQLClient delegate){
+        return new AsyncJooqSQLClientImpl(vertx, delegate);
+    }
+
+    /**
+     *
+     * @param query
+     * @param mapper
+     * @param <P>
+     * @return
+     */
+    <P> CompletableFuture<List<P>> fetch(Query query, Function<JsonObject, P> mapper);
+
+    /**
+     * @param query a jOOQ-query
+     * @param mapper a function to map the result into another object.
+     * @param <P>
+     * @return A CompletableFuture returning on object of P or <code>null</code>.
+     */
+    <P> CompletableFuture<P> fetchOne(Query query, Function<JsonObject, P> mapper);
+
+    /**
+     * @param query a jOOQ-query
+     * @return A CompletableFuture returning the number of affected rows by this query.
+     */
+    CompletableFuture<Integer> execute(Query query);
+
+    /**
+     * @return the underlying client
+     */
+    AsyncSQLClient delegate();
+}
---
+++ vertx-jooq-async-generate/src/test/java/io/github/jklingsporn/vertx/jooq/generate/async/future/VertxSomethingDaoTest.java
@@ -0,0 +1,91 @@
+package io.github.jklingsporn.vertx.jooq.generate.async.future;
+
+import generated.future.async.vertx.Tables;
+import generated.future.async.vertx.tables.pojos.Something;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.jooq.impl.DSL;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by jensklingsporn on 13.06.17.
+ */
+public class VertxSomethingDaoTest extends VertxAsyncDaoTestBase {
+
+
+    @Test
+    public void asyncCRUDExecShouldSucceed() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Something somethingCreated = createSomething();
+        dao.insertExecAsync(somethingCreated).
+                thenAccept(created -> Assert.assertEquals(1L, created.longValue())).
+                thenCompose(v -> dao.fetchOneAsync(Tables.SOMETHING.SOMEHUGENUMBER.eq(somethingCreated.getSomehugenumber()))).
+                thenCompose(fetch -> dao.updateExecAsync(somethingCreated.setSomestring("modified").setSomeid(fetch.getSomeid()))).
+                thenAccept(updated -> Assert.assertEquals(1L, updated.longValue())).
+                thenCompose(v -> dao.deleteExecAsync(somethingCreated.getSomeid())).
+                thenAccept(deleted -> Assert.assertEquals(1L, deleted.longValue())).
+                whenComplete(failOrCountDown(latch));
+        await(latch);
+    }
+
+    @Test
+    public void insertExecShouldSucceed() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        dao.insertExecAsync(createSomething()).
+                thenAccept(insertedRows -> Assert.assertEquals(1L,insertedRows.longValue())).
+                thenCompose(v-> dao.client().fetchOne(DSL.using(dao.configuration()).selectFrom(Tables.SOMETHING).orderBy(Tables.SOMETHING.SOMEID.desc()).limit(1),dao.jsonMapper())).
+                thenCompose(id -> {
+                    Assert.assertNotNull(id);
+                    return dao.deleteExecAsync(id.getSomeid());
+
+                }).
+                thenAccept(deletedRows -> Assert.assertEquals(1L,deletedRows.longValue())).
+                whenComplete(failOrCountDown(latch));
+        await(latch);
+    }
+
+//    @Test
+//    public void testReturning(){
+//        CountDownLatch latch = new CountDownLatch(1);
+//        dao.client().delegate().getConnection(sqlConnection->{
+//            if(sqlConnection.failed()){
+//                sqlConnection.cause().printStackTrace();
+//            }
+//            sqlConnection.result().execute("insert into something values(null,null,null,null,null,null,null,null,null)", ex -> {
+//                if (ex.failed()) {
+//                    ex.cause().printStackTrace();
+//                }
+//                sqlConnection.result().query("select LAST_INSERT_ID()", lastId -> {
+//                    if (lastId.failed()) {
+//                        lastId.cause().printStackTrace();
+//                    } else {
+//                        Assert.assertEquals(1L, lastId.result().getResults().size());
+//                        System.out.println(lastId.result().toJson());
+//                        latch.countDown();
+//                    }
+//                });
+//            });
+//        });
+//        await(latch);
+//    }
+
+
+
+    private Something createSomething(){
+        Random random = new Random();
+        Something something = new Something();
+        something.setSomestring("test");
+        something.setSomedouble(random.nextDouble());
+        something.setSomehugenumber(random.nextLong());
+        something.setSomeregularnumber(random.nextInt());
+        something.setSomejsonarray(new JsonArray().add(1).add(2).add(3));
+        something.setSomejsonobject(new JsonObject().put("key", "value"));
+        something.setSomesmallnumber((short) random.nextInt(Short.MAX_VALUE));
+        return something;
+    }
+
+}
